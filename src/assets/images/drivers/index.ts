6618a8e106bbdb299c88f484f3f1eefe273199e6
Pouria Maleki <dev.maleki@gmail.com>
2018-12-01 17:49:36 +0330

    fix raikkonen image

--- src/assets/images/drivers/index.ts
+++ src/assets/images/drivers/index.ts
@@ -1,5 +1,5 @@
 import driverImageFernando from "./alonso.png";
-import driverImageKimi from "./raikkonen.jpg";
+import driverImageKimi from "./raikkonen.png";
 import driverImageHamilton from "./hamilton.png";
 import driverImageVettel from "./vettel.png";
 import driverImageButton from "./button.png";
